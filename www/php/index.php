eefaf492a2dfe2459c5935d4a7bd790689c9a6ca
DEVAsriel <ddoggest@gmail.com>
2017-01-31 20:20:08 +0300

    ok

--- www/php/index.php
+++
@@ -1,8 +0,0 @@
-<<?php
-if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == off;){
-  $redirect = 'https://' . $_SERVER['HTTPS_HOST'] . $_SERVER['REQUEST.URL']
-  header('HTTP/1.1 301 Moved Permanently');
-  header('Location: ' . $redirect)
-  exit();
-}
-?>
